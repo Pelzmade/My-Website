551b0323445d74f14d8036a884009d26f9c592e9
Pelzmade <147931630+Pelzmade@users.noreply.github.com>
2024-04-20 15:39:41 -0600

    java

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ button.addEventListener('click', () => {
     
     alert('Button clicked!');
 });
+
+<script>
+window.location.href = "https://pelzmade.github.io/cprg-218-final-project/";
+</script>
