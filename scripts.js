bafd47c16e7c99ac10b768c4408f403c538c4f8d
Pelzmade <147931630+Pelzmade@users.noreply.github.com>
2024-04-21 20:17:33 -0600

    fixed java & audio file

--- scripts.js
+++ scripts.js
@@ -7,6 +7,11 @@ button.addEventListener('click', () => {
     alert('Button clicked!');
 });
 
+function buttonClick() {
+    alert("Nice to meet you");
+}
+
 <script>
 window.location.href = "https://pelzmade.github.io/cprg-218-final-project/";
+
 </script>
