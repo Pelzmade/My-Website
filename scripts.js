a9f9772ecce6d5589487574302bf81f4d65a8310
Pelzmade <147931630+Pelzmade@users.noreply.github.com>
2024-09-02 10:56:26 -0600

    new edits

--- scripts.js
+++ scripts.js
@@ -12,5 +12,37 @@ function buttonClick() {
 }
 
 
-window.location.href = "https://pelzmade.github.io/cprg-218-final-project/";
+window.location.href = "https://pelzmade.github.io/My-Website/";
+
+
+
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
+  const carousel = document.querySelector('.carousel');
+  const images = document.querySelectorAll('.carousel-image');
+  let index = 0;
+
+  function showImage(index) {
+    const totalImages = images.length;
+    if (index >= totalImages) {
+      index = 0;
+    } else if (index < 0) {
+      index = totalImages - 1;
+    }
+    carousel.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  prevButton.addEventListener('click', () => {
+    index--;
+    showImage(index);
+  });
+
+  nextButton.addEventListener('click', () => {
+    index++;
+    showImage(index);
+  });
+
+  showImage(index); // Initial display
+
+
 
